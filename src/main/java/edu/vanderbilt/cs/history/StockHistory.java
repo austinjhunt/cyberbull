3fbe644c2bba6fe90cd0110e5b93400d345f0d87
Austin Hunt <huntaj@g.cofc.edu>
2021-07-11 20:25:48 -0400

    flesh out stockhistory class

--- src/main/java/edu/vanderbilt/cs/history/StockHistory.java
+++ src/main/java/edu/vanderbilt/cs/history/StockHistory.java
@@ -5,5 +5,24 @@
 
 package edu.vanderbilt.cs.history;
 
+import edu.vanderbilt.cs.Stock;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+/*
+StockHistory class uses an underlying hashmap data structure to map historical datetimes to the price of a specific
+stock at that datetime.
+ */
+
 public class StockHistory {
+    private HashMap<LocalDateTime, Double> priceHistory;
+    private Stock stock;
+    public StockHistory(Stock stock){
+        this.stock = stock;
+        this.priceHistory = new HashMap<>();
+    }
+    public void addRecord(LocalDateTime dateTime, double price){
+        this.priceHistory.put(dateTime, price);
+    }
 }
--- src/main/java/edu/vanderbilt/cs/history/StockHistoryVisitor.java
+++ src/main/java/edu/vanderbilt/cs/history/StockHistoryVisitor.java
@@ -7,7 +7,8 @@ package edu.vanderbilt.cs.history;
 
 /*
 Visitor interface that uses visitor design pattern to build out different
-types of Stock Histories (which vary by time interval and length).
+types of Stock Histories (which vary by time interval and length). Each implementation will use YahooFinance API
+to pull information with different intervals.
  */
 public interface StockHistoryVisitor {
     public StockHistory visit();
