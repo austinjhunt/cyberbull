4f7182abf4f27d63526efdc7fdb28317c6a82be7
Austin Hunt <huntaj@g.cofc.edu>
2021-08-04 14:48:40 -0400

    try failing test

--- src/test/java/edu/vanderbilt/cs/cyberbull/core/position/PositionTest.java
+++ src/test/java/edu/vanderbilt/cs/cyberbull/core/position/PositionTest.java
@@ -21,6 +21,7 @@ class PositionTest {
         double expected = stock.getCurrentPrice() * quantity;
         double lowVal = expected - 10;
         double highVal = expected + 10;
+        assertTrue(false);
         assertTrue(expected > lowVal && expected < highVal);
     }
 
